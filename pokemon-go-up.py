3310ebb1c03eb8bcda13e0a7205842d198f5f5d9
Isaac Norman <isaacdnorman@gmail.com>
2016-07-13 17:11:40 +1000

    Small typo fixes

--- pokemon-go-up.py
+++ pokemon-go-up.py
@@ -4,8 +4,8 @@ import requests
 try:
     time = requests.get('https://pgorelease.nianticlabs.com/plfe/').elapsed.total_seconds()
     if time < 3:
-        print "Go! catch them all!"
+        print "Go! Catch them all!"
     else:
         print "Servers are down, go back to work"
 except:
-    print "Go! Cacth them all!"
+    print "Go! Catch them all!"
