dc03e9f9746c9617f731050844135ddfffc7fc41
ariestiyansyah <ariestiyansyah.rizky@gmail.com>
2016-07-13 08:37:52 +0700

    fix missing #!/usr/bin/env python

--- pokemon-go-up.py
+++ pokemon-go-up.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python
 import requests
 # using requests library
 try:
-    time = requets.get('https://pgorelease.nianticlabs.com/plfe/').elapsed.total_seconds()
+    time = requests.get('https://pgorelease.nianticlabs.com/plfe/').elapsed.total_seconds()
     if time < 3:
         print "Go! catch them all!"
     else:
--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-
 setup(
     name='pokemon-go-up',
-    version='1.3',
+    version='1.3.4',
     scripts=['pokemon-go-up.py'],
     packages=find_packages(exclude=['tests*']),
     license='MIT',
